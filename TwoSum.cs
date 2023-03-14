241b30d8167c2cf60610acdd93848a5fa2d662b2
Emma <emmanuelponciano@gmail.com>
2023-03-14 18:47:36 -0400

    TwoSum Solution

---
+++ TwoSum.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    //Space: O(n)
+    //Time: O(n)
+    public int[] TwoSum(int[] nums, int target)
+    {
+        if (nums.Length <= 1) return new int[] { };
+
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int result = target - nums[i];
+            if (dict.ContainsKey(result)) return new int[] { i, dict[result] };
+            dict[nums[i]] = i;
+        }
+
+        return null;
+    }
+}
