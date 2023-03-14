959fb67d7fd90eb94ef59e4e1c2b5f0edded4460
Emma <emmanuelponciano@gmail.com>
2023-03-14 19:05:39 -0400

    Contains Duplicate Solution

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+
+public class Solution
+{
+    //Space: O(n)
+    //Time: O(n)
+    public bool ContainsDuplicate(int[] nums)
+    {
+        if (nums.Length <= 1) return false;
+
+        var hashSet = new HashSet<int>();
+        foreach (var item in nums)
+        {
+            if (hashSet.Contains(item))
+            {
+                return true;
+            }
+            else
+            {
+                hashSet.Add(item);
+            }
+        }
+
+        return false;
+    }
+}
