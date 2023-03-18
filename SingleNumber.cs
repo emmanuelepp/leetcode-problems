c974e565c0da17ba0ad44ee2bfafae269d58d7c7
Emma <emmanuelponciano@gmail.com>
2023-03-18 19:59:50 -0400

    Single Number Solution

---
+++ SingleNumber.cs
@@ -0,0 +1,19 @@
+//O(n)
+//o(1)
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!dict.ContainsKey(nums[i])) dict[nums[i]] = i;
+            else dict.Remove(nums[i]);
+        }
+
+        return dict.Keys.First();
+    }
+}
