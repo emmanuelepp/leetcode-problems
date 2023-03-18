6c4503b6e8921dca94325c4efccbf90c2ae90930
Emma <emmanuelponciano@gmail.com>
2023-03-18 18:08:44 -0400

    Is Anagram Solution

---
+++ IsAnagram.cs
@@ -0,0 +1,21 @@
+//O(n)
+//O(n)
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var dict = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            dict.TryAdd(s[i], 0);
+            dict.TryAdd(t[i], 0);
+
+            dict[s[i]]++;
+            dict[t[i]]--;
+        }
+
+        return dict.Values.All(x => x == 0);
+    }
+}
