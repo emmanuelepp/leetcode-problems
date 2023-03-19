767694dcde44918eac045c8c907c2a11195236ff
Emma <emmanuelponciano@gmail.com>
2023-03-19 15:48:53 -0400

    Two Sum Sorted Solucion

---
+++ TwoSum-SortedArr.cs
@@ -0,0 +1,30 @@
+//O(n)
+//O(n)
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+
+        if (numbers == null || numbers.Length <= 0) return new int[] { };
+
+        int L = 0, R = numbers.Length - 1;
+
+        while (L < R)
+        {
+            if (numbers[L] + numbers[R] == target)
+            {
+                return new int[] { L + 1, R + 1 };
+            }
+            else if (numbers[L] + numbers[R] < target)
+            {
+                L++;
+            }
+            else
+            {
+                R--;
+            }
+        }
+
+        return new int[] { };
+    }
+}
--- TwoSum.cs
+++ TwoSum.cs
@@ -14,6 +14,6 @@ public class Solution
             dict[nums[i]] = i;
         }
 
-        return null;
+        return new int[] { };
     }
 }
