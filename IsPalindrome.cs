a69084372f1895dd54bcf2464d7fcc185e574dc6
Emma <emmanuelponciano@gmail.com>
2023-03-19 19:11:38 -0400

    Is Palindrome Solution

---
+++ IsPalindrome.cs
@@ -0,0 +1,31 @@
+using System.Text.RegularExpressions;
+
+//O(n)
+//O(n)
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        if (s == null || string.IsNullOrEmpty(s)) return false;
+
+        var regex = new Regex("[^a-zA-Z0-9]");
+        s = regex.Replace(s.ToLower(), "");
+
+        int L = 0, R = s.Length - 1;
+
+        while (L < R)
+        {
+            if (s[L] == s[R])
+            {
+                L++;
+                R--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
