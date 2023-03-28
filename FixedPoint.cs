d770fae15971d8198f79df12701aad3f1f4b95db
Emma <emmanuelponciano@gmail.com>
2023-03-28 19:11:51 -0400

    Fixed Point improve Solution

--- FixedPoint.cs
+++ FixedPoint.cs
@@ -1,18 +1,32 @@
-//O(n)
+//O(logn)
 //O(1)
-public class Solution
-{
-    public int FixedPoint(int[] arr)
-    {
-        if (arr.Length <= 0) return -1;
+public class Solution {
+    public int FixedPoint(int[] arr) {
 
-        for (int i = 0; i < arr.Length; i++)
+        if (arr == null || arr.Length <= 0) return -1;
+
+        int L = 0, R = arr.Length -1;
+        int result = -1;
+
+        while(L <= R)
         {
-            if (arr[i] == i)
+            int mid = L + (R - L) / 2;
+
+            if (arr[mid] == mid)
             {
-                return arr[i];
+                result = mid;
+                R = mid - 1;
+            } 
+            if (arr[mid] < mid)
+            {   
+                L = mid + 1;
+            }
+            else
+            {
+                R = mid - 1;
             }
         }
-        return -1;
+
+        return result;
     }
 }
