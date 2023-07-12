649ad515b5c2640651ab691bcffeac73952822b3
Emma <emmanuelponciano@gmail.com>
2023-07-12 11:49:46 -0400

    Unique Occurrences Solution

---
+++ UniqueOccurrences.cs
@@ -0,0 +1,28 @@
+//O(n)
+//O(1)
+
+using System.Collections;
+
+public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        var countDict = new Dictionary<int, int>();
+
+        foreach (int num in arr)
+        {
+            if (countDict.ContainsKey(num))
+            {
+                countDict[num]++;
+            }
+            else
+            {
+                countDict[num] = 1;
+            }
+        }
+
+        var occurrenceSet = new HashSet<int>(countDict.Values);
+
+        return countDict.Count == occurrenceSet.Count;
+    }
+}
