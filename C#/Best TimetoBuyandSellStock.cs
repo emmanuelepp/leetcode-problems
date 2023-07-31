57ca090c85c7f5deb55c9f393b4573c2e1dba9e2
Emma <emmanuelponciano@gmail.com>
2023-07-31 19:21:09 -0400

    Best time to buy a stock.

---
+++ C#/Best TimetoBuyandSellStock.cs
@@ -0,0 +1,29 @@
+//O(n)
+//O(1)
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 0 || prices.Length == 1) return 0;
+
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+
+            int posibleProfit = prices[i] - minPrice;
+
+            if (posibleProfit > maxProfit)
+            {
+                maxProfit = posibleProfit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
