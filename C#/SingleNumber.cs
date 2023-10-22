ee2003c0f8b8dcb4d4aec291152eceaabb493cb7
Emma <emmanuelponciano@gmail.com>
2023-10-21 20:26:12 -0400

    fix

--- C#/SingleNumber.cs
+++ C#/SingleNumber.cs
@@ -4,17 +4,14 @@ public class Solution
 {
     public int SingleNumber(int[] nums)
     {
-        public int SingleNumber(int[] nums)
+        if (nums.Length <= 0) return -1;
+
+        var result = 0;
+
+        foreach (var item in nums)
         {
-            if (nums.Length <= 0) return -1;
-
-            var result = 0;
-
-            foreach (var item in nums)
-            {
-                result ^= item;
-            }
-            return result;
+            result ^= item;
         }
+        return result;
     }
 }
