973870f29f42717fda216c6e94891803f75691ae
Emma <emmanuelponciano@gmail.com>
2023-10-23 16:45:24 -0400

    fix

--- C#/TwoSum.cs
+++ C#/TwoSum.cs
@@ -1,16 +1,23 @@
+//O(N)
+//O(N)
 public class Solution
 {
-    //O(n)
-    //O(n)
     public int[] TwoSum(int[] nums, int target)
     {
-        if (nums.Length <= 1) return new int[] { };
+
+        if (nums.Length == 0) return new int[] { };
+
         var dict = new Dictionary<int, int>();
-        
+
         for (int i = 0; i < nums.Length; i++)
         {
             int result = target - nums[i];
-            if (dict.ContainsKey(result)) return new int[] { i, dict[result] };
+
+            if (dict.ContainsKey(result))
+            {
+                return new int[] { dict[result], i };
+            }
+
             dict[nums[i]] = i;
         }
 
