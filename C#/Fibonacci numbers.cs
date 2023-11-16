d880110495ad51197be76386f4b3db6fc6d2bb1d
Emma <emmanuelponciano@gmail.com>
2023-11-15 20:14:07 -0500

    Fibo Solucion

---
+++ C#/Fibonacci numbers.cs
@@ -0,0 +1,18 @@
+public class Solution
+{
+
+    private Dictionary<int, int> dictMemo = new Dictionary<int, int>();
+
+    public int Fib(int n)
+    {
+
+        if (n <= 1) return n;
+
+        if (dictMemo.ContainsKey(n))
+            return dictMemo[n];
+
+        dictMemo[n] = Fib(n - 1) + Fib(n - 2);
+
+        return dictMemo[n];
+    }
+}
