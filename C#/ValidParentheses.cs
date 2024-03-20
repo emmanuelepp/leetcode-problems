9d4979edd315cd2afafa334835b4424f16ddac07
Emma <emmanuelponciano@gmail.com>
2024-03-20 17:24:10 -0400

    IsValid more simple solution

--- C#/ValidParentheses.cs
+++ C#/ValidParentheses.cs
@@ -1,34 +1,20 @@
 //O(n)
-//O(n)
+//O(1)
 public class Solution
 {
     public bool IsValid(string s)
     {
 
-        if (s == null) return false;
+        if (s.Length == 0) return false;
 
         var stack = new Stack<char>();
 
-        var map = new Dictionary<char, char> {
-            { ')', '(' },
-            { '}', '{' },
-            { ']', '[' }
-        };
-
-        foreach (char c in s)
+        foreach (var c in s)
         {
-            if (map.ContainsKey(c))
-            {
-                if (stack.Count == 0 || stack.Peek() != map[c])
-                {
-                    return false;
-                }
-                stack.Pop();
-            }
-            else
-            {
-                stack.Push(c);
-            }
+            if (c == '(') stack.Push(')');
+            else if (c == '{') stack.Push('}');
+            else if (c == '[') stack.Push(']');
+            else if (stack.Count == 0 || stack.Pop() != c) return false;
         }
 
         return stack.Count == 0;
