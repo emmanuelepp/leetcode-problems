2569c6a7a053028cbf0a7d2fc439d82902236e1b
Emma <emmanuelponciano@gmail.com>
2024-03-22 22:34:46 -0400

    create a better solution

--- C#/RemoveVowels.cs
+++ C#/RemoveVowels.cs
@@ -1,21 +1,22 @@
+
+//O(n)
+//O(1)
 public class Solution
 {
-    //O(n)
-    //O(1)
     public string RemoveVowels(string s)
     {
         if (string.IsNullOrWhiteSpace(s)) return "";
 
-        var result = new List<string>();
+        var result = "";
 
         for (int i = 0; i < s.Length; i++)
         {
             if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
             {
-                result.Add(s[i].ToString());
+                result += s[i];
             }
         }
 
-        return string.Join("", result);
+        return result;
     }
 }
