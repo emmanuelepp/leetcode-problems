3fcbb2d3f30749ed8ff5addac8083e3cba0752f0
emmanuelepp <emmanuelponciano@gmail.com>
2024-10-27 20:18:52 -0400

    reverseList solution

---
+++ C++/reverseList.cpp
@@ -0,0 +1,38 @@
+// Time: O(N)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode *current = head;
+        ListNode *prev = nullptr;
+        ListNode *next = nullptr;
+
+        while (current != nullptr)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+
+        return head;
+    }
+};
