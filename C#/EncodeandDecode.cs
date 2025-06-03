2010a6b3a2749d6e4aad324e7906d432c09d0816
emmanuelepp <emmanuelponciano@gmail.com>
2025-06-03 16:28:06 -0400

    Encode and  Decode Solution

---
+++ C#/EncodeandDecode.cs
@@ -0,0 +1,27 @@
+//O(n)
+//O(n)
+
+public class Solution
+{
+    public string Encode(IList<string> strs)
+    {
+        var sb = new System.Text.StringBuilder();
+        foreach (var str in strs)
+            sb.Append(str.Length).Append('#').Append(str);
+        return sb.ToString();
+    }
+
+    public List<string> Decode(string s)
+    {
+        var result = new List<string>();
+        int i = 0;
+        while (i < s.Length)
+        {
+            int j = s.IndexOf('#', i);
+            int len = int.Parse(s.Substring(i, j - i));
+            result.Add(s.Substring(j + 1, len));
+            i = j + 1 + len;
+        }
+        return result;
+    }
+}
